44286373260774e35e345b427aa3a14b4cd39cdf
H4xN-Harsh <harshxvillen74@gmail.com>
2025-07-23 17:51:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full border-2 border-violet-600 bg-gray-600'>
+      <h1 className='text-center text-7xl text-blue-500 mt-2'>Background Color Changer </h1>
+    </div>
+  )
+}
+
+export default App
